266d1ffa4569000111a3eb55f7e5946aed7f337f
Matej Grcic <m.j.grcic7@gmail.com>
2017-11-11 18:28:13 +0100

    TimeSeriesPrediction added.

--- src/main/java/hr/fer/zemris/projekt/predictions/tdnn/INeuralNetworkTrainer.java
+++ src/main/java/hr/fer/zemris/projekt/predictions/tdnn/INeuralNetworkTrainer.java
@@ -2,5 +2,5 @@ package hr.fer.zemris.projekt.predictions.tdnn;
 
 public interface INeuralNetworkTrainer {
 
-    void trainNeuralNetwork(INeuralNetwork neuralNetwork, IErrorCalculator errorProvider);
+    void trainNeuralNetwork(INeuralNetwork neuralNetwork, IErrorCalculator errorCalculator);
 }

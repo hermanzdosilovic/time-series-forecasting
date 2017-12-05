225a888dadc5bec8125a7e7bf291518548845437
marinsokol5 <marin.sokol@fer.hr>
2017-12-05 12:41:28 +0100

    weird changes

--- src/main/java/hr/fer/zemris/projekt/predictions/models2/ARTest.java
+++ src/main/java/hr/fer/zemris/projekt/predictions/models2/ARTest.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Supplier;
@@ -13,53 +15,42 @@ import java.util.stream.Collectors;
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 
 public class ARTest {
-	
-	public static void main(String[] args){
-		double[] dataset;
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(
-				new FileInputStream(new File
-						("C:\\Users\\JARVIS\\Desktop\\Luka faks\\Peti semestar\\ARDataset.txt"))))){
-			
-			Supplier<List<String>> supplier = () -> new LinkedList<String>();
-				
-				List<String> b = br.lines().collect(Collectors.toCollection(supplier));
-				int i = 0;
-				dataset = new double[b.size()];
-				for (String s : b) {
-					double value = Double.parseDouble(s.split(" ")[4]);
-					int k = (int)(value * 100);
-					int l = k % 1000;
-					value = l / 100.;
-//					System.out.println(value + ",");
-					dataset[i] = value;
-					i++;
-				}
-		}catch(IOException e){
-			System.out.println(e);
-			return;
-		}
-		
-		double mean = new Mean().evaluate(dataset);
-		
+
+    public static void main(String[] args) throws IOException {
+        List<String> b = Files.readAllLines(Paths.get("./datasets/ARDataset.txt"));
+        int i = 0;
+        double[] dataset = new double[b.size()];
+        for (String s : b) {
+            double value = Double.parseDouble(s.split(" ")[4]);
+            int k = (int) (value * 100);
+            int l = k % 1000;
+            value = l / 100.;
+            System.out.println(value + ",");
+            dataset[i] = value;
+            i++;
+        }
+
+        double mean = new Mean().evaluate(dataset);
+
 		/*for (int i = 0; i < dataset.length; i++) {
 			dataset[i] -= mean;
 		}*/
-		
-		AR model = new AR(200, dataset, false);
-		model.fitModel();
-		
-		System.out.println(model.forecast(1));
-		System.out.println(model.getAIC());
-//		System.out.println(model.checkIfStationary());
-		
-		System.out.println(model.coeffs()[0]);
-//		MA model = new MA(2, dataset, false);
-//		System.out.println(model.getAIC(new double[] {0.1537}));
-//		model.fitModel();
-//		
-//		for (double d : model.getCoeff().getColumnPackedCopy()) {
-//			System.out.println(d);
-//		}
-//		
-	}
+
+        AR model = new AR(1, dataset, false);
+        model.fitModel();
+
+        System.out.println(model.forecast(1));
+        System.out.println(model.getAIC());
+        //		System.out.println(model.checkIfStationary());
+
+        System.out.println(model.coeffs()[0]);
+        //		MA model = new MA(2, dataset, false);
+        //		System.out.println(model.getAIC(new double[] {0.1537}));
+        //		model.fitModel();
+        //
+        //		for (double d : model.getCoeff().getColumnPackedCopy()) {
+        //			System.out.println(d);
+        //		}
+        //
+    }
 }
--- src/main/java/hr/fer/zemris/projekt/predictions/test/TestAR.java
+++ src/main/java/hr/fer/zemris/projekt/predictions/test/TestAR.java
@@ -1,70 +1,56 @@
-package hr.fer.zemris.projekt.predictions.test;
-
-import hr.fer.zemris.projekt.predictions.models.AR;
-import hr.fer.zemris.projekt.predictions.models.ModelUtil;
-import hr.fer.zemris.projekt.predictions.util.Graph;
-import hr.fer.zemris.projekt.predictions.util.Util;
-import hr.fer.zemris.projekt.predictions.util.UtilException;
-
-import javax.swing.*;
-import java.awt.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
-import hr.fer.zemris.projekt.predictions.models.ModelUtil;
-import hr.fer.zemris.projekt.predictions.util.Graph;
-import hr.fer.zemris.projekt.predictions.util.Util;
-import hr.fer.zemris.projekt.predictions.util.UtilException;
-
-import javax.swing.*;
-import java.awt.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
-public class TestAR extends JFrame {
-
-    public TestAR() {
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("TestAR");
-        setSize(640, 480);
-
-        initGUI();
-    }
-
-    private void initGUI() {
-        Container cp = getContentPane();
-        cp.setLayout(new BorderLayout());
-
-        Path path = Paths.get("./src/main/resources/ARDataset.txt");
-        JPanel panel;
-        try {
-            Graph graph = new Graph();
-
-            List<Double> ar_data = Util.parseDataset(path, 3, " ");
-            List<Double> ar_data_stationary = ModelUtil.stationarize(ar_data);
-            //graph.addNewData(ar_data_stationary, "AR data");
-
-            AR ar = new AR(1, ar_data_stationary);
-            ar.computeNextValues(200);
-            graph.addNewData(ar.getData(), "AR predicted");
-
-            panel = Util.graphAsPanel(graph);
-        } catch (UtilException e) {
-            cp.add(new JLabel(String.format(
-                    "Exception %s happened.",
-                    e.getMessage()
-            )), BorderLayout.CENTER);
-            return;
-        }
-
-        cp.add(panel, BorderLayout.CENTER);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new TestAR().setVisible(true);
-        });
-    }
-}
+//package hr.fer.zemris.projekt.predictions.test;
+//
+//import hr.fer.zemris.project.forecasting.models.ModelUtil;
+//import hr.fer.zemris.projekt.predictions.models.AR;
+//import hr.fer.zemris.project.forecasting.util.Util;
+//import javax.swing.*;
+//import java.awt.*;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+//public class TestAR extends JFrame {
+//
+//    public TestAR() {
+//        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//        setTitle("TestAR");
+//        setSize(640, 480);
+//
+//        initGUI();
+//    }
+//
+//    private void initGUI() {
+//        Container cp = getContentPane();
+//        cp.setLayout(new BorderLayout());
+//
+//        Path path = Paths.get("./src/main/resources/ARDataset.txt");
+//        JPanel panel;
+//        try {
+//            Graph graph = new Graph();
+//
+//            List<Double> ar_data = Util.parseDataset(path, 3, " ");
+//            List<Double> ar_data_stationary = ModelUtil.stationarize(ar_data);
+//            //graph.addNewData(ar_data_stationary, "AR data");
+//
+//            AR ar = new AR(1, ar_data_stationary);
+//            ar.computeNextValues(200);
+//            graph.addNewData(ar.getData(), "AR predicted");
+//
+//            panel = Util.graphAsPanel(graph);
+//        } catch (UtilException e) {
+//            cp.add(new JLabel(String.format(
+//                    "Exception %s happened.",
+//                    e.getMessage()
+//            )), BorderLayout.CENTER);
+//            return;
+//        }
+//
+//        cp.add(panel, BorderLayout.CENTER);
+//    }
+//
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(() -> {
+//            new TestAR().setVisible(true);
+//        });
+//    }
+//}

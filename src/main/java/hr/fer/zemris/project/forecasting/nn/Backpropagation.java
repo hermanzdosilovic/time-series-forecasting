45c1ba36d5ee5799bd6d22f14cbc2c75dc3c890c
Matej Grcic <m.j.grcic7@gmail.com>
2017-12-29 15:57:15 +0100

    Backpropagation util created.

--- src/main/java/hr/fer/zemris/project/forecasting/nn/Backpropagation.java
+++ src/main/java/hr/fer/zemris/project/forecasting/nn/Backpropagation.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static hr.fer.zemris.project.forecasting.nn.util.BackpropagationUtil.*;
+
 public class Backpropagation {
 
     private List<DatasetEntry> trainingSet;
@@ -199,49 +201,5 @@ public class Backpropagation {
 
     public void removeObserver(NeuralNetworkObserver observer) {
         observers.remove(observer);
-    }
-
-    private static RealMatrix[] createLayerWeights(double[] weights, int[] architecture) {
-        RealMatrix[] layerWeights = new RealMatrix[architecture.length - 1];
-        int offset = 0;
-        for (int i = 0; i < architecture.length - 1; ++i) {
-            RealMatrix layer = new Array2DRowRealMatrix(architecture[i] + 1, architecture[i + 1]);
-            for (int j = 0; j < architecture[i] + 1; ++j) {
-                double[] rowWeight = Arrays.copyOfRange(weights, offset, offset + architecture[i + 1]);
-                offset += architecture[i + 1];
-                layer.setRow(j, rowWeight);
-            }
-            layerWeights[i] = layer;
-        }
-        return layerWeights;
-    }
-
-    private static double[] extractWeights(RealMatrix[] layerWeights, int weightsNumber) {
-        double[] weights = new double[weightsNumber];
-        int offset = 0;
-        for (RealMatrix layerWeight : layerWeights) {
-            for (int j = 0; j < layerWeight.getRowDimension(); ++j) {
-                double[] row = layerWeight.getRow(j);
-                System.arraycopy(row, 0, weights, offset, row.length);
-                offset += row.length;
-            }
-        }
-        return weights;
-    }
-
-    private static List<DatasetEntry>[] createBatches(int batchSize, List<DatasetEntry> trainingSet) {
-        int batchNumber = (int) Math.ceil(trainingSet.size() / (double) batchSize);
-        List<DatasetEntry>[] batches = (List<DatasetEntry>[]) new List[batchNumber];
-        for (int i = 0; i < batchNumber; i++) {
-            if (i < batchNumber - 1) {
-                batches[i] = trainingSet.subList(i * batchSize, (i + 1) * batchSize);
-            } else {
-                int len = trainingSet.size() % batchSize;
-                len = len == 0 ? 32 : len;
-                batches[i] = trainingSet.subList(i * batchSize, i * batchSize + len);
-            }
-        }
-
-        return batches;
     }
 }
